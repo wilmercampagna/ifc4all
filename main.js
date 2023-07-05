92e527127d3c00b1338d9dedf97fe1ace12a3bf0
wilmercampagna <36424603+wilmercampagna@users.noreply.github.com>
2023-07-05 11:21:49 -0500

    UPDATE_05_JUL
    Update with pwa service

--- main.js
+++ main.js
@@ -77,6 +77,18 @@ document.querySelector('#app').innerHTML = `
   </div>
 </div>
 `
+// The service worker
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then( (registration) => {
+    console.log("SW registered")
+    console.log(registration)
+  }).catch( (error) => {
+    console.log("SW registration failed")
+    console.log(error)
+  })
+}
+
 //Creates the Three.js scene
 const scene = new Scene();
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
 import { comlink } from 'vite-plugin-comlink'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,38 @@ export default defineConfig({
   base: 'https://wilmercampagna.github.io/ifc4all',
   worker: {
     plugins: [
+        VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: {
+            enabled: true
+          },
+          workbox: {
+            clientsClaim: true,
+            skipWaiting: true,
+            globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+          },
+          manifest: {
+            "name": "ifc4all",
+            "short_name": "ifc4all",
+            "start_url": ".",
+            "background_color": "#ffffff",
+            "theme_color": "#ffffff",
+            "display": "standalone",
+            "images": [
+              {
+                "src": "public/grua.png",
+                "sizes": "192x192",
+                "type": "image/png",
+                "purpose": "maskable"
+              },
+              {
+                "src": "public/grua500.png",
+                "sizes": "512x512",
+                "type": "image/png"
+              }
+            ] 
+          }
+        }),
         comlink(),
         // comlink({ useModuleWorker: true })
     ],
